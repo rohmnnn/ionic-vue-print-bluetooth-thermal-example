0080d5042420c8aa02b33c3829efb72fefe20ffc
Muhammad Fatkhurrohman <hi@rohman.dev>
2025-09-14 10:42:02 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-vue-printing-bluetooth-serial-example',
+  webDir: 'dist'
+};
+
+export default config;
